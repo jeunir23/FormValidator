b294a19bf08a8ec1c26641ba60a7768929e8bc05
jeunir23 <jeunir23@gmail.com>
2024-09-03 19:59:49 +0800

    Cells was added

--- FormValidator.js
+++ FormValidator.js
@@ -13,4 +13,9 @@ form.addEventListener("submit", function (event) {
     .getElementsByTagName("tbody")[0];
 
   const newRow = table.insertRow();
+
+  const cell1 = newRow.insertCell(0);
+  const cell2 = newRow.insertCell(1);
+  const cell3 = newRow.insertCell(2);
+  const cell4 = newRow.insertCell(3);
 });
