705054c7fad39602586a4615ff0c3bc4e252a669
jeunir23 <jeunir23@gmail.com>
2024-09-03 19:50:19 +0800

    Getting input values

--- FormValidator.js
+++ FormValidator.js
@@ -2,4 +2,9 @@ const form = document.getElementsById("infoForm");
 
 form.addEventListener("submit", function (event) {
   event.preventDefault();
+
+  const name = document.getElementById("name").value;
+  const birthdate = document.getElementById("name").value;
+  const age = document.getElementById("name").value;
+  const contact = document.getElementById("name").value;
 });
