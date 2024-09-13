bdcf495a79a53516824a73087c827d2bb4838fd2
jeunir23 <jeunir23@gmail.com>
2024-09-13 14:30:21 +0800

    Delete button in row

--- FormValidator.js
+++ FormValidator.js
@@ -18,9 +18,16 @@ form.addEventListener("submit", function (event) {
   const cell2 = newRow.insertCell(1);
   const cell3 = newRow.insertCell(2);
   const cell4 = newRow.insertCell(3);
+  const cell5 = newRow.insertCell(4);
 
   cell1.textContent = name;
   cell2.textContent = birthdate;
   cell3.textContent = age;
   cell4.textContent = number;
+  cell5.innerHTML = '<button onclick="deleteRow(this)">Delete</button>';
 });
+
+function deleteRow(evt) {
+  const deleteIndex = evt.parentElement.parentElement.rowIndex;
+  document.getElementById("myTable").deleteRow(deleteIndex);
+}
